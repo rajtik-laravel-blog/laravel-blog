6a40d584b8b178b29ddd7f22737810e52b5cc944
Vladislav Rajtmajer <rajtik.laravel.blog@gmail.com>
2026-02-12 10:52:48 +0100

    refactor: footer re-design

--- resources/js/components/BlogFooter.vue
+++ resources/js/components/BlogFooter.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
-import { privacyPolicy } from '@/routes';
+<script lang="ts" setup>
+import {Link} from '@inertiajs/vue3';
+import {privacyPolicy} from '@/routes';
 </script>
 
 <template>
@@ -8,12 +8,13 @@ import { privacyPolicy } from '@/routes';
         class="border-t border-[#19140015] py-10 text-center text-xs text-[#706f6c] dark:border-[#2a2a27] dark:text-[#A1A09A]"
     >
         <div class="mx-auto max-w-7xl px-6">
-            <p>
-                Vytvořeno v
-                <span class="font-medium text-[#f53003] dark:text-[#FF4433]"
-                    >Laravel</span
-                > &copy; {{ new Date().getFullYear() }} by Vladislav Rajtmajer.
-                <span class="mx-2 text-[#19140015] dark:text-[#2a2a27]">|</span>
+            <p class="flex justify-between">
+                <span>
+                    Vytvořeno v
+                    <span class="font-medium text-[#f53003] dark:text-[#FF4433]"
+                    ><a href="https://laravel.com">Laravel</a></span
+                    > &copy; {{ new Date().getFullYear() }} by Vladislav Rajtmajer
+                </span>
                 <Link
                     :href="privacyPolicy().url"
                     class="hover:text-[#1b1b18] dark:hover:text-[#EDEDEC] transition-colors"
